5e0d5cb11b8950cb41c70900ea0bbcb04e0e42bd
Exetria <107202768+Exetria@users.noreply.github.com>
2022-06-18 10:09:50 +0700

    Update Player.java

--- Project PBO/core/src/com/project/game/Player.java
+++ Project PBO/core/src/com/project/game/Player.java
@@ -6,20 +6,23 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.graphics.Texture;
 
-public class Player extends Rectangle{
+public class Player extends Rectangle
+{
     private int hp;
     private int xp;
     private int score;
     private int laserDmg;
 
-    Player(){
+    Player()
+    {
         this.hp = 100;
         this.xp = 0;
         this.score = 0;
         this.laserDmg = 10;
     }
 
-    public void menerimadamage(int damage) {
+    public void menerimadamage(int damage)
+    {
         this.hp -= damage;
     }
 
